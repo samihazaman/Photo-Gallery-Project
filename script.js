a8527fa30d0343159a28d3aa575c47f37daf6a3c
samihazaman <75856425+samihazaman@users.noreply.github.com>
2020-12-15 14:17:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+$( document ).ready(function() {
+
 var flowers = ["https://images.unsplash.com/photo-1561826336-37bdb1339994?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80", "https://images.unsplash.com/photo-1603387464762-3edd559871e5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=633&q=80", "https://images.unsplash.com/photo-1519064438923-de4de326dfd1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1868&q=80"];
 
 flowers.forEach(function(item, picture) {
@@ -16,4 +18,5 @@ $(".images").append("<img src=" + flowers[picture] + ">");
 
 });
 
+});
 
